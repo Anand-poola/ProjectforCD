97a920e228347d3f2244b1b17e4b4b54c3ea64a4
ANANDKUMAR <poola@GMAIL.COM>
2025-11-20 11:37:57 +0530

    Projectown

---
+++ devops-demo-app-updated/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Together Eswar and Anand have completed this DevOps project. Congrats to Both!";
+    }
+}
